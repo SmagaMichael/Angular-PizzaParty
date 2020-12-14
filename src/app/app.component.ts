5da0e7c3b7dfc5b8221734ba236a7378e8d2b394
Smaga Michaele <smaga.michael@bbox.fr>
2020-12-14 15:04:34 +0100

    début

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Pizza } from './pizza';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pizzaparty';
+  name = '4 fromages';
+
+  // maSuperPizza est typée avec la chaine Pizza
+  maSuperPizza : Pizza ={
+    id:1,
+    name : '4 fromages',
+    price : 12,
+    image :'4-fromages.jpg'
+  };
 }
---
+++ src/app/pizza.ts
@@ -0,0 +1,8 @@
+
+// cette classe me permet de structure ce que contient une pizza
+export class Pizza {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+}
